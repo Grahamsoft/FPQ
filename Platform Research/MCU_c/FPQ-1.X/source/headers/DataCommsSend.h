d04da3b9e2b29c3c6f72309ca926d2ee80f11be0
David Graham <david@oninmedia.com>
2013-06-13 00:55:25 +0100

    Daily Checkin

---
+++ Platform Research/MCU_c/FPQ-1.X/source/headers/DataCommsSend.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   DataCommsSend.h
+ * Author: David
+ *
+ * Created on June 4, 2013, 11:39 PM
+ */
+
+#ifndef DATACOMMSSEND_H
+#define	DATACOMMSSEND_H
+
+void CustomerWating( uint8_t theKeyId );
+void CustomerBeingServed( uint8_t theKeyId );
+
+#endif	/* DATACOMMSSEND_H */
+
